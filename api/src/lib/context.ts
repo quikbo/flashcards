670e36c42642e0047cbd4a1fcc0a0b973e2ad151
williamb301 <wboudy1@jhu.edu>
2024-08-07 14:09:23 -0400

    initialized web and api folders

---
+++ api/src/lib/context.ts
@@ -0,0 +1,10 @@
+import { Env } from "hono";
+import { Session, User } from "lucia";
+
+
+export interface Context extends Env {
+    Variables: {
+        user: User | null;
+        session: Session | null;
+    };
+}
